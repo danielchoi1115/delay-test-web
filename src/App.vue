e7494512a9a22fada41d1d0487ef8820edb4522d
Daniel Choi <schoiak@connect.ust.hk>
2022-09-14 22:38:07 +0800

    Mod - main app edited

--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,66 @@
 <template>
+  <div @click="startInterval()">Click Me</div>
+  <p>gravity: {{ grav }}</p>
+  <p>scale: {{ sca }}</p>
+  <p>ball y: {{ ballY }}</p>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-2">
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://github.com/quasarframework/"
-        >
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://chat.quasar.dev"
-        >
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://forum.quasar.dev"
-        >
-          <q-item-section avatar>
-            <q-icon name="forum" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item
-          clickable
-          tag="a"
-          target="_blank"
-          href="https://twitter.com/quasarframework"
-        >
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <HelloWorld />
+      <BounceHome>
+        <template v-slot:ball>
+          <BouncingBall />
+        </template>
+        <template v-slot:floor>
+          <BounceFloor />
+        </template>
+        <template v-slot:slider>
+          <SpeedSlider />
+        </template>
+      </BounceHome>
     </q-page-container>
   </q-layout>
 </template>
 
-<script>
-import { ref } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+<style>
+body {
+  margin: 0;
+  background-color: #024;
+  color: white;
+  height: 100vh;
+  position: relative;
+}
+</style>
 
+<script>
+import { ref } from 'vue'
+import { BounceHome } from './views'
+import { BouncingBall, BounceFloor, SpeedSlider } from './components'
+import { mapGetters } from 'vuex'
+import { moveBall } from './controllers'
 export default {
-  name: "LayoutDefault",
+  name: 'LayoutDefault',
 
   components: {
-    HelloWorld,
+    BounceHome,
+    BouncingBall,
+    BounceFloor,
+    SpeedSlider,
   },
-
+  computed: mapGetters({
+    ballY: 'getBallYpx',
+    grav: 'getGravity',
+    sca: 'getScale',
+  }),
   setup() {
     return {
       leftDrawerOpen: ref(false),
-    };
+    }
   },
-};
+  methods: {
+    startInterval() {
+      setInterval(function () {
+        moveBall()
+      }, 500)
+    },
+  },
+}
 </script>
