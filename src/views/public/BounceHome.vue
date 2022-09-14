46e726635e49dc840161fb433107c4470720ead1
Daniel Choi <schoiak@connect.ust.hk>
2022-09-14 22:36:43 +0800

    Add - view added

---
+++ src/views/public/BounceHome.vue
@@ -0,0 +1,15 @@
+<template>
+  <!--부모에서 정의한 '왼쪽 버튼'이 위치합니다 -->
+  <slot name="ball"></slot>
+
+  <!--부모에서 정의한 '오른쪽 버튼'이 위치합니다 -->
+  <slot name="floor"></slot>
+
+  <slot name="slider"></slot>
+</template>
+
+<script>
+export default {
+  name: 'BounceHome',
+}
+</script>
