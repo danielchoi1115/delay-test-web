26c502f998e0f1c973f492fdc3ed05d8ea0562db
Daniel Choi <schoiak@connect.ust.hk>
2022-09-14 22:37:35 +0800

    Mod - linted by eslint

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: ["quasar"],
+  transpileDependencies: ['quasar'],
 
   pluginOptions: {
     quasar: {
-      importStrategy: "kebab",
+      importStrategy: 'kebab',
       rtlSupport: false,
     },
   },
-});
+})
