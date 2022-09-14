79c6f3f9dbf7d52f993d6c0727346042f4bbac23
Daniel Choi <schoiak@connect.ust.hk>
2022-09-14 22:35:57 +0800

    Add - index for component

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import BouncingBall from './public/BouncingBall.vue'
+import BounceFloor from './public/BounceFloor.vue'
+import SpeedSlider from './public/SpeedSlider.vue'
+export { BouncingBall, BounceFloor, SpeedSlider }
