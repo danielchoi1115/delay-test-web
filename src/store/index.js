5de57b03bdb72d45c9b225c6bb55b7dbd01a7328
Daniel Choi <schoiak@connect.ust.hk>
2022-09-15 16:40:05 +0800

    Mod - new states added

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,75 @@
 import { createStore } from 'vuex'
-
+// https://codepen.io/bramus/full/DEMQLg
 export default createStore({
   state: {
-    ballY: 0,
-    gravity: 1,
-    scale: 1,
+    ball_diameter: 50,
+    ball_y: 0,
+    speed_y: 0,
+    acc_y: 3,
+    min_acc_y: 0,
+    max_acc_y: 500,
+    acc_y_step: 10,
+    bottom: 250,
+    bpm: 100,
+    current_intv_id: 0,
   },
   getters: {
+    getBallDiameter: state => {
+      return state.ball_diameter / 10 + 'rem'
+    },
     getBallY: state => {
-      return state.ballY
+      return state.ball_y / 10 + 'rem'
     },
-    getBallYpx: state => {
-      return state.ballY + 'px'
+    getAccY: state => {
+      return state.acc_y / 10 + 'rem'
     },
-    getGravity: state => {
-      return state.gravity
+    getBottom: state => {
+      return state.bottom / 10 + 'rem'
     },
-    getScale: state => {
-      return state.scale
+    getSpeedY: state => {
+      return state.speed_y / 10 + 'rem'
+    },
+    getMinAccY: state => {
+      return state.min_acc_y
+    },
+    getMaxAccY: state => {
+      return state.max_acc_y
+    },
+    getAccYStep: state => {
+      return state.acc_y_step
+    },
+    getBPM: state => {
+      return state.bpm
+    },
+    getCurrentIntvID: state => {
+      return state.current_intv_id
     },
   },
   mutations: {
-    calculateNewBallY(state) {
-      state.ballY = state.ballY + state.gravity
+    updateBallY(state) {
+      state.ball_y = state.ball_y + state.speed_y
     },
-    increaseGravity(state) {
-      state.gravity += state.scale
+    accerateBallY(state) {
+      state.speed_y += state.acc_y
     },
-    checkGravity(state) {
-      if (state.ballY > 420) {
-        state.gravity = -state.gravity
+    updateAccY(state) {
+      if (state.ball_y >= state.bottom) {
+        state.speed_y = -state.speed_y
+      } else {
+        state.speed_y += state.acc_y
       }
+      state.ball_y = state.ball_y + state.speed_y
     },
-    setScale(state, value) {
-      state.scale = value
+    setAccY(state, value) {
+      state.acc_y = value
+      state.speed_y = 0
+      state.ball_y = 0
+    },
+    setCurrentIntvID(state, value) {
+      state.current_intv_id = value
+    },
+    setBPM(state, value) {
+      state.bpm = value
     },
   },
   actions: {},
