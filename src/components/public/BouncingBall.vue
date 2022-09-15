3bcaeb7397c0434c9cce419ac6b85061f6c53956
Daniel Choi <schoiak@connect.ust.hk>
2022-09-15 16:39:07 +0800

    Mod - added binding with store value

--- src/components/public/BouncingBall.vue
+++ src/components/public/BouncingBall.vue
@@ -6,14 +6,14 @@
 <style>
 .ball {
   background-color: white;
-  width: 50px;
-  height: 50px;
+  width: v-bind('ball_diameter');
+  height: v-bind('ball_diameter');
   position: absolute;
   border-radius: 50%;
   box-shadow: 0 0 15px 5px #08f;
   left: 50%;
-  top: v-bind('ballY');
-  transform: translateX(-50%);
+  top: v-bind('ball_y');
+  transition: all 0.01s linear;
 }
 </style>
 <script>
@@ -22,7 +22,8 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'BouncingBall',
   computed: mapGetters({
-    ballY: 'getBallYpx',
+    ball_y: 'getBallY',
+    ball_diameter: 'getBallDiameter',
   }),
   data() {
     return {
