b0d1fabbb4b547a9c157ec99c60579151ac53d4e
Daniel Choi <schoiak@connect.ust.hk>
2022-09-16 22:40:36 +0800

    Mod - gsap animation added

--- src/components/public/SpeedSlider.vue
+++ src/components/public/SpeedSlider.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="q-pa-md">
-    <q-badge color="secondary"> Model: {{ scale }} ({{ min_acc_y }} to {{ max_acc_y }}) </q-badge>
+    <q-badge color="secondary"> Model: {{ scale }} ({{ min_bpm }} to {{ max_bpm }}) </q-badge>
     <q-slider
       v-model="scale"
       @change="setBPM(scale)"
-      :min="min_acc_y"
-      :max="max_acc_y"
-      :step="acc_y_step"
+      :min="min_bpm"
+      :max="max_bpm"
+      :step="bpm_step"
+      markers
+      snap
       color="green"
     />
   </div>
+  <div>
+    BPM
+    <p class="big-number">{{ tweened.toFixed(0) }}</p>
+  </div>
 </template>
+<style>
+.big-number {
+  font-weight: bold;
+  font-size: 2em;
+}
+</style>
 
 <script>
 import { ref } from 'vue'
 import { mapGetters } from 'vuex'
+import gsap from 'gsap'
 
 export default {
   name: 'SpeedSlider',
@@ -25,14 +38,25 @@ export default {
   },
   computed: mapGetters({
     // arrow functions can make the code very succinct!
-    min_acc_y: 'getMinAccY',
-    max_acc_y: 'getMaxAccY',
-    acc_y_step: 'getAccYStep',
+    min_bpm: 'getMinAccY',
+    max_bpm: 'getMaxAccY',
+    bpm_step: 'getAccY_pxStep',
   }),
   setup() {
     return {
       scale: ref(10),
     }
+  },
+  data() {
+    return {
+      number: 0,
+      tweened: 0,
+    }
+  },
+  watch: {
+    scale(n) {
+      gsap.to(this, { duration: 0.5, tweened: Number(n) || 0 })
+    },
   },
 }
 </script>
