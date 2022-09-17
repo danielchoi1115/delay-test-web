e9c2af642db5c9f154f9b6db2d6602552bac6671
Daniel Choi <schoiak@connect.ust.hk>
2022-09-17 16:42:29 +0800

    Mod - pulse animation added

--- src/components/public/BounceFloor.vue
+++ src/components/public/BounceFloor.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="floor">
-    <!-- <span class="info">Y : 0, G : 0</span> -->
-  </div>
+  <div class="floor" ref="floor"></div>
+
+  <button @click="pulse">hahahahaha</button>
 </template>
 <style>
 .floor {
@@ -10,11 +10,52 @@
   height: 20px;
   position: absolute;
   box-shadow: 0 0 15px 5px #08f;
-  top: 500px;
+  top: 460px;
+  left: calc(v-bind('ball_diameter') * 0.5px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
 }
 </style>
 <script>
+import store from '@/store'
+import { createApp } from 'vue'
+import { mapGetters, mapState } from 'vuex'
+import PurseEffectVue from './PurseEffect.vue'
 export default {
   name: 'BouncingBall',
+  computed: {
+    ...mapState(['ball_y', 'ball_diameter']),
+    ...mapGetters({
+      interval: 'getInterval',
+    }),
+  },
+  data() {
+    return {
+      bottom: store.getters.getBottom,
+      intv: 0,
+    }
+  },
+  watch: {
+    interval(i) {
+      this.intv = i
+    },
+    ball_y(y) {
+      if (y >= this.bottom) {
+        const floor = this.$refs.floor
+        setTimeout(function () {
+          var ComponentApp = createApp(PurseEffectVue)
+          const wrapper = document.createElement('div')
+          wrapper.setAttribute(
+            'style',
+            'display: flex; justify-content: center; align-items: center;align-content: center;',
+          )
+          ComponentApp.mount(wrapper)
+          floor.appendChild(wrapper)
+        }, this.interval)
+      }
+    },
+  },
 }
 </script>
---
+++ src/components/public/PurseEffect.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="purse"></div>
+</template>
+<style>
+.purse {
+  border: 5px solid rgb(0, 75, 151);
+  opacity: 0;
+  width: 30px;
+  height: 100px;
+  border-radius: 50%;
+  position: absolute;
+}
+</style>
+<script>
+import store from '@/store'
+
+export default {
+  name: 'PurseEffect',
+  data() {
+    return {
+      ball_diameter: store.getters.getBallDiameter_px,
+    }
+  },
+  mounted() {
+    const duration = 1000
+    const ripple_animation = [
+      { width: '0', opacity: 1, height: '0', offset: 0 },
+      { opacity: 0.5, offset: 0.7 },
+      { width: '300px', height: '100px', opacity: 0, offset: 1 },
+    ]
+
+    const ripple_timing = {
+      duration: duration,
+      iterations: 1,
+    }
+
+    const pulse = this.$el
+    pulse.animate(ripple_animation, ripple_timing)
+    setTimeout(function () {
+      pulse.parentNode.remove()
+    }, duration)
+  },
+}
+</script>
