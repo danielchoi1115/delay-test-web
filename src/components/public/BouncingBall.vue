8768d9308781b8d02651733d4ff3a018dd9552ef
Daniel Choi <schoiak@connect.ust.hk>
2022-09-17 16:42:42 +0800

    Test - animation migration to css

--- src/components/public/BouncingBall.vue
+++ src/components/public/BouncingBall.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ball">
+  <div class="ball" @click="startMoving()">
     <!-- <span class="info">Y : 0, G : 0</span> -->
   </div>
 </template>
@@ -12,20 +12,53 @@
   border-radius: 50%;
   box-shadow: 0 0 15px 5px #08f;
   left: 50%;
-  top: v-bind('ball_y');
-  transition: v-bind('transition');
+  /* top: calc(v-bind('ball_y') * 1px); */
+  /* transition: v-bind('transition'); */
+  z-index: 999;
+  animation: bounce;
+  animation-duration: 3s;
+  animation-direction: alternate;
+  animation-timing-function: cubic-bezier(0.3, 0, 0.85, 0.5);
+  animation-iteration-count: infinite;
+}
+@keyframes bounce {
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  100% {
+    transform: translate3d(0, 420px, 0);
+  }
 }
 </style>
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 import store from '@/store'
 export default {
   name: 'BouncingBall',
-  computed: mapGetters({
-    ball_y: 'getBallY_px',
-    transition: 'getTransition',
-  }),
-  methods: {},
+  computed: {
+    ...mapState(['ball_y']),
+    ...mapGetters({
+      transition: 'getTransition',
+    }),
+  },
+  methods: {
+    startMoving() {
+      const duration = 1000
+      const ripple_animation = [
+        { top: 0, offset: 0 },
+        { top: '420px', offset: 0.5 },
+        { top: 0, offset: 1 },
+      ]
+
+      const ripple_timing = {
+        duration: duration,
+        iterations: Infinity,
+      }
+
+      const pulse = this.$el
+      pulse.animate(ripple_animation, ripple_timing)
+    },
+  },
   data() {
     return {
       colorFromScript: 'white',
