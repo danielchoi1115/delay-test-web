d51a500caf1fe31797b0c52b9559f439c85172e6
Daniel Choi <schoiak@connect.ust.hk>
2022-09-17 16:41:56 +0800

    Mod - eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
         useSpaces: true,
         tabWidth: 2,
         trailingComma: 'all',
-        printWidth: 120,
+        printWidth: 100,
         bracketSpacing: true,
         arrowParens: 'avoid',
         endOfLine: 'auto',
